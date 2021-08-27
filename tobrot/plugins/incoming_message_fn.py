6e05d8d8cacfaddff37d2c53aeda2bd94523515b
Judhistir <70930235+judhistir88@users.noreply.github.com>
2021-08-27 21:38:12 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -138,7 +138,7 @@ async def incoming_message_f(client, message):
             await i_m_sefg.edit_text(err_message)
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "What have you entered. \nPlease read /help \n"
             f"<b>API Error</b>: {cf_name}"
         )
 
@@ -198,7 +198,7 @@ async def incoming_youtube_dl_f(client, message):
             await i_m_sefg.edit_text(text=text_message, reply_markup=reply_markup)
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "What have you entered. \nPlease read /help \n"
             f"<b>API Error</b>: {cf_name}"
         )
 
