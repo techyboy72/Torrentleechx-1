32d59b27460443e55764fecb01daa449b0fd8eaa
Judhistir <70930235+judhistir88@users.noreply.github.com>
2021-09-08 09:35:55 +0530

    Update call_back_button_handler.py

--- tobrot/plugins/call_back_button_handler.py
+++ tobrot/plugins/call_back_button_handler.py
@@ -26,26 +26,26 @@ async def button(bot, update: CallbackQuery):
         chat_id, mes_id, from_usr = cmf[1], cmf[2], cmf[3]
         if (int(update.from_user.id) == int(from_usr)) or g:
             await bot.answer_callback_query(
-                update.id, text="trying to cancel...", show_alert=False
+                update.id, text="Trying to cancel...", show_alert=False
             )
             gDict[int(chat_id)].append(int(mes_id))
         else:
             await bot.answer_callback_query(
                 callback_query_id=update.id,
-                text="You R Not Allowed",
+                text="U Naughty 😁, U R Not Allowed",
                 show_alert=True,
                 cache_time=0,
             )
         return
     if "|" in cb_data:
         await bot.answer_callback_query(
-            update.id, text="trying to download...", show_alert=False
+            update.id, text="Trying to download...", show_alert=False
         )
         await youtube_dl_call_back(bot, update)
         return
     if cb_data.startswith("rclone"):
         await bot.answer_callback_query(
-            update.id, text="choose rclone config...", show_alert=False
+            update.id, text="Choose rclone config...", show_alert=False
         )
         await rclone_button_callback(bot, update)
         return
@@ -53,11 +53,11 @@ async def button(bot, update: CallbackQuery):
     if cb_data.startswith("cancel"):
         if (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
             await bot.answer_callback_query(
-                update.id, text="trying to cancel...", show_alert=False
+                update.id, text="Trying to cancel...", show_alert=False
             )
             if len(cb_data) > 1:
                 i_m_s_e_g = await update.message.reply_to_message.reply_text(
-                    "checking..?", quote=True
+                    "Scanning...", quote=True
                 )
                 aria_i_p = await aria_start()
                 g_id = cb_data.split()[-1]
@@ -76,21 +76,21 @@ async def button(bot, update: CallbackQuery):
                         else:
                             os.remove(file_name)
                     await i_m_s_e_g.edit_text(
-                        f"Leech Cancelled by <a href='tg://user?id={update.from_user.id}'>{update.from_user.first_name}</a>"
+                        f"Leech Cancelled by <a> href='tg://user?id={update.from_user.id}'>{update.from_user.first_name} So Tata GdBye Khatam</a>"
                     )
                 except Exception as e:
                     await i_m_s_e_g.edit_text("<i>FAILED</i>\n\n" + str(e) + "\n#error")
         else:
             await bot.answer_callback_query(
                 callback_query_id=update.id,
-                text="U R Not Allowed",
+                text="U Naughty 😁, U R Not Allowed",
                 show_alert=True,
                 cache_time=0,
             )
     elif cb_data == "fuckingdo":
         if (update.from_user.id in AUTH_CHANNEL) or g:
             await bot.answer_callback_query(
-                update.id, text="trying to delete...", show_alert=False
+                update.id, text="Trying to delete...", show_alert=False
             )
             g_d_list = [
                 "app.json",
@@ -133,9 +133,9 @@ async def button(bot, update: CallbackQuery):
             else:
                 await update.message.edit_text("Nothing to clear 🙄")
         else:
-            await update.message.edit_text("You are not allowed to do that 🤭")
+            await update.message.edit_text("U Naughty 😁, U R Not Allowed 🤭")
     elif cb_data == "fuckoff":
         await bot.answer_callback_query(
-            update.id, text="trying to cancel...", show_alert=False
+            update.id, text="Trying to cancel...", show_alert=False
         )
         await update.message.edit_text("Okay! fine 🤬")
